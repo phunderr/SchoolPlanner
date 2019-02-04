27dde0505f93ebd1241b93b6975cf7bd1b6a4ef5
Jelmer Surewaard <Jelmer.surewaard@gmail.com>
2019-02-04 10:14:55 +0100

    removed HEAD

--- src/SchoolPlanner/Main.java
+++ src/SchoolPlanner/Main.java
@@ -3,10 +3,5 @@ package SchoolPlanner;
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-	//Test test
-=======
-        System.out.println("yeet");
->>>>>>> Lars
     }
 }
