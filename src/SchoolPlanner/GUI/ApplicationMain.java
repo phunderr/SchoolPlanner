aafb09cfd68a7077d1e0cabd16c0f6c0070c9ec4
Lars <lplh.vangisbergen@student.avans.nl>
2019-02-04 10:14:50 +0100

    setup test

--- src/SchoolPlanner/GUI/ApplicationMain.java
+++ src/SchoolPlanner/GUI/ApplicationMain.java
@@ -3,11 +3,16 @@ package SchoolPlanner.GUI;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
 public class ApplicationMain extends javafx.application.Application {
+
+    private BorderPane mainPane;
+    private GridPane mainGrid;
 
 
     public static void main(String[] args) {
@@ -15,11 +20,20 @@ public class ApplicationMain extends javafx.application.Application {
 
     }
     public void start(Stage stage){
-        BorderPane testPane = new BorderPane();
-        Scene testScene = new Scene(testPane);
+        initialize(stage);
+        gridEditor();
+    }
 
-
+    public void initialize(Stage stage){
+        this.mainPane = new BorderPane();
+        this.mainGrid = new GridPane();
+        mainPane.setCenter(mainGrid);
+        Scene testScene = new Scene(mainPane);
         stage.setScene(testScene);
         stage.show();
+    }
+
+    public void gridEditor(){
+        mainGrid.add(new Label("skrrr"),0,0);
     }
 }
