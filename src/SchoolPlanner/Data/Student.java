a816939f6eb7ee6bafd9727119ce96bcdba65c1c
Arno Nagtzaam <arno32@live.nl>
2019-02-04 12:33:27 +0100

    Student edited
    student edited

--- src/SchoolPlanner/Data/Student.java
+++ src/SchoolPlanner/Data/Student.java
@@ -2,14 +2,7 @@ package SchoolPlanner.Data;
 
 public class Student extends Person {
 
-    private int studentNumber;
-
-    public Student (String name, Gender gender, int studentNumber) {
+    public Student(String name, Gender gender, int studentNumber) {
         super(name, gender);
-        this.studentNumber = studentNumber;
-    }
-
-    public int getStudentNumber () {
-        return studentNumber;
     }
 }
