4e8a8fbb4bac258f96917dd2132309cef3fd9cc3
phunderr <pm.holthuijsen@student.avans.nl>
2019-02-12 12:42:17 +0100

    Person class fixed

--- src/SchoolPlanner/Data/Person.java
+++ src/SchoolPlanner/Data/Person.java
@@ -1,6 +1,14 @@
 package SchoolPlanner.Data;
 
-public class Person {
+public abstract class Person {
 
     private String name;
+
+    public Person (String name) {
+        this.name = name;
+    }
+
+    public String getName () {
+        return name;
+    }
 }
