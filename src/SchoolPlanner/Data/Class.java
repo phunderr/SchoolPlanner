481b1e76e88c891e0c4e3338aee1a92a0921b4c7
phunderr <pm.holthuijsen@student.avans.nl>
2019-02-12 12:29:16 +0100

    Class changhes

--- src/SchoolPlanner/Data/Class.java
+++ src/SchoolPlanner/Data/Class.java
@@ -6,4 +6,11 @@ public class Class implements Serializable {
 
     private String classID;
 
+    public Class (String classID) {
+        this.classID = classID;
+    }
+
+    public String getClassID () {
+        return classID;
+    }
 }
--- src/SchoolPlanner/Data/Student.java
+++ src/SchoolPlanner/Data/Student.java
@@ -2,6 +2,7 @@ package SchoolPlanner.Data;
 
 public class Student extends Person {
 
+    //not sure if neccesary if not used can be removed
     public Student(String name) {
         super(name);
     }
