6d1cb504cc2fafaaf1026e1eb283beea64871022
phunderr <pm.holthuijsen@student.avans.nl>
2019-02-25 12:35:14 +0100

    Commands added

--- src/SchoolPlanner/GUI/Scenes/MainScene.java
+++ src/SchoolPlanner/GUI/Scenes/MainScene.java
@@ -9,7 +9,6 @@ import SchoolPlanner.GUI.Logics.LessonRectangle;
 import SchoolPlanner.GUI.Logics.RectangleLogics;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
