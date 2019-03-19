8f78641e3775cb53944eeb041ecf78513dd93305
Jelmer Surewaard <Jelmer.surewaard@gmail.com>
2019-03-19 11:51:07 +0100

    wip wip

--- src/simulation/tiled/Layer.java
+++ src/simulation/tiled/Layer.java
@@ -27,13 +27,12 @@ public class Layer {
         this.layer = layer;
         tilesets = bf;
         JsonArray data = layer.getJsonArray("data");
-        height = layer.getInt("height");
-        width = layer.getInt("width");
-
+//        height = layer.getInt("height");
+//        width = layer.getInt("width");
         tiles = new ArrayList<>();
         int index = 0;
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
+        for (int y = 0; y < 100; y++) {
+            for (int x = 0; x < 100; x++) {
 
                 tiles.add(new Tile(new Point2D.Double(x * 32, y * 32), getImageByIndex(data.getInt(index))));
                 index++;
@@ -45,17 +44,25 @@ public class Layer {
         int tileWidth = 32;
         int tileHeight = 32;
         int collumnWidth = 0;
+        int firstGrid = 0;
+        int tilecount = 0;
+        int dataID = 0;
         BufferedImage layerImage = tilesets.get(0);
         if(index == 1){
             return layerImage.getSubimage(tileWidth * (index % 8), tileHeight * (index / 8), tileWidth, tileHeight);
         }
+
         for (int i = 0; i < tilesets.size(); i++){
             layerImage = tilesets.get(i);
             collumnWidth = Integer.parseInt((jsonValues.getJsonObject(i).get("columns").toString()));
-            index -= 1;
-            return index < 1 ? null : layerImage.getSubimage(tileWidth * (index % collumnWidth), tileHeight * (index / collumnWidth), tileWidth, tileHeight);
+            firstGrid = Integer.parseInt((jsonValues.getJsonObject(i).get("firstgid").toString()));
+            tilecount = Integer.parseInt((jsonValues.getJsonObject(i).get("tilecount").toString()));
+            dataID = firstGrid + tilecount;
+
+            return index < dataID ? null : layerImage.getSubimage(tileWidth * (index % collumnWidth), tileHeight * (index % collumnWidth), tileWidth, tileHeight);
         }
-       return null;
+        index -= 1;
+        return index < 1 ? null : layerImage.getSubimage(tileWidth * (index % collumnWidth), tileHeight * (index % collumnWidth), tileWidth, tileHeight);
     }
 
     public ArrayList<Tile> getTiles() {
