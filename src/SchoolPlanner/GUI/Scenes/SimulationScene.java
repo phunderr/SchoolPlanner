8c96666cc8d50d68c262decfd720b34f05c2b59d
phunderr <pm.holthuijsen@student.avans.nl>
2019-03-26 11:16:34 +0100

    error fixes

--- src/SchoolPlanner/GUI/Scenes/SimulationScene.java
+++ src/SchoolPlanner/GUI/Scenes/SimulationScene.java
@@ -25,28 +25,31 @@ public class SimulationScene {
     private Map map;
     private ArrayList<Character> characterArrayList;
     private Camera camera;
-    private Point2D startingPoint = new Point2D.Double(960,1000);
+    private Point2D startingPoint = new Point2D.Double(960, 1000);
 
 
-    public BorderPane simulationScene (){
+    public BorderPane simulationScene () {
+        init();
         BorderPane mainPane = new BorderPane();
         this.canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
         canvas.setHeight(1080);
         canvas.setWidth(1920);
-        FXGraphics2D g2d =new FXGraphics2D(canvas.getGraphicsContext2D());
-        map = new Map("/maps/SchoolPlannerMap.json");
+        FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
+
         this.characterArrayList = new ArrayList<>();
         characterArrayList.add(new Character());
 
 
         new AnimationTimer() {
             long last = -1;
+
             @Override
             public void handle (long now) {
-                if ( last == -1)
+                if ( last == -1 ) {
                     last = now;
-                update((now - last) * 1.0e9);
+                }
+                update(( now - last ) * 1.0e9);
                 last = now;
                 draw(g2d);
             }
@@ -58,37 +61,45 @@ public class SimulationScene {
     }
 
 
-    public void draw(FXGraphics2D g){
+    public void draw (FXGraphics2D g) {
         g.setTransform(new AffineTransform());
         g.setBackground(Color.BLACK);
-        g.clearRect(0, 0, (int)canvas.getWidth(), (int)canvas.getHeight());
-        g.setTransform(camera.getTransform((int) canvas.getWidth(), (int) canvas.getHeight()));
+        g.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        if ( camera != null ) {
+            g.setTransform(camera.getTransform((int) canvas.getWidth(), (int) canvas.getHeight()));
 
-        map.draw(g);
-        for (Character character : this.characterArrayList) {
-            character.draw(g);
+            map.draw(g);
+            for ( Character character : this.characterArrayList ) {
+                character.draw(g);
+            }
         }
     }
 
-    public void update(double deltaTime){
+    public void update (double deltaTime) {
         loadinCharacters();
-        for (Character character : characterArrayList) {
+        for ( Character character : characterArrayList ) {
             character.update(characterArrayList);
         }
 
     }
-    private void loadinCharacters(){
-        if(characterArrayList.size() < 10){
-            if(characterArrayList.get(characterArrayList.size()-1).getDistance(startingPoint) > 32) {
+
+    private void loadinCharacters () {
+        if ( characterArrayList.size() < 10 ) {
+            if ( characterArrayList.get(characterArrayList.size() - 1).getDistance(startingPoint) > 32 ) {
                 characterArrayList.add(new Character());
             }
         }
         boolean containsteacher = false;
-        for (Character character : characterArrayList) {
-            if (character instanceof Teacher){
+        for ( Character character : characterArrayList ) {
+            if ( character instanceof Teacher ) {
                 containsteacher = true;
             }
         }
+    }
+
+    public void init () {
+        this.camera = null;
+        map = new Map("/maps/SchoolPlannerMap.json");
     }
 
 }
